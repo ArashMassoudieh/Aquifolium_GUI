6ff4253c566eba0e00faea3f8d1f17f4389fd525
Unknown <arashmassoudieh@gmail.com>
2018-08-20 22:40:52 -0400

    GraphicsScene

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "QDebug"
+#include "QGraphicsRectItem"
+
 #define cout qDebug()
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -10,20 +12,39 @@ MainWindow::MainWindow(QWidget *parent) :
     system.GetQuanTemplate(qApp->applicationDirPath().toStdString() + "/resources/power_reservoirs.qnt");
     ui->setupUi(this);
     qDebug()<<qApp->applicationDirPath();
-    qDebug()<<"Salam!";
-    qDebug()<<"Salam!";
 
-    for (int i=0; i<system.GetAllBlockTypes().size(); i++)
+
+    for (unsigned int i=0; i<system.GetAllBlockTypes().size(); i++)
     {
         qDebug()<<QString::fromStdString(system.GetAllBlockTypes()[i]);
         QAction* action = new QAction(this);
         action->setObjectName(QString::fromStdString(system.GetAllBlockTypes()[i]));
         QIcon icon;
-        icon.addFile(QString::fromStdString(qApp->applicationDirPath().toStdString()+"/resources/Icons/"+system.GetMetaModel()[system.GetAllBlockTypes()[i]]->IconFileName()), QSize(), QIcon::Normal, QIcon::Off);
+        icon.addFile(QString::fromStdString(qApp->applicationDirPath().toStdString()+"/resources/Icons/"+system.GetModel(system.GetAllBlockTypes()[i])->IconFileName()), QSize(), QIcon::Normal, QIcon::Off);
         action->setIcon(icon);
         ui->mainToolBar->addAction(action);
         action->setText(QString::fromStdString(system.GetAllBlockTypes()[i]));
     }
+    ui->mainToolBar->addSeparator();
+    for (unsigned int i=0; i<system.GetAllLinkTypes().size(); i++)
+    {
+        qDebug()<<QString::fromStdString(system.GetAllLinkTypes()[i]);
+        QAction* action = new QAction(this);
+        action->setObjectName(QString::fromStdString(system.GetAllLinkTypes()[i]));
+        QIcon icon;
+        icon.addFile(QString::fromStdString(qApp->applicationDirPath().toStdString()+"/resources/Icons/"+system.GetModel(system.GetAllLinkTypes()[i])->IconFileName()), QSize(), QIcon::Normal, QIcon::Off);
+        action->setIcon(icon);
+        ui->mainToolBar->addAction(action);
+        action->setText(QString::fromStdString(system.GetAllLinkTypes()[i]));
+    }
+
+    QGraphicsScene *scene = new QGraphicsScene();
+    ui->graphicsView_2->setScene(scene);
+
+    QGraphicsRectItem* item = new QGraphicsRectItem(0,0,100,100);
+    item->setBrush(QBrush(Qt::red));
+    item->setFlags(QGraphicsItem::ItemIsMovable);
+    ui->graphicsView_2->scene()->addItem(item);
 
 }
 
